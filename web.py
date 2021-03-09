1654f005d6f96eb263e8bd0831309c48c93b6f9e
XuanShine <xuan.ng@hotmail.com>
2021-03-09 20:18:55 +0100

    isolate part pyaccess

--- web.py
+++ web.py
@@ -1,23 +1,60 @@
-import sys, os
-import logging
-import time
-C = os.path.abspath(os.path.dirname(__file__))
+from datetime import datetime
 
-logging.basicConfig(filename=os.path.join(C, "run_server.log"), level=logging.INFO, format="%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s")
+from bottle import run, template, Bottle, Response, route
+from bottle import jinja2_view
 
-from pyaccess_server import app
+import lock_door
 
-sys.path.append(C)
-from pyaccess_server import views
-app.register_blueprint(views)
+pyaccess = Bottle()
+
+
+@pyaccess.route("/help")
+def help():
+    return """
+    "/"
+    "/get_state/<relai_ch>" 
+    "/turn/<state>/<relai_ch>
+    """
+
+
+@pyaccess.route("/get_state/<relai_ch>")
+def get_state(relai_ch):
+    return
+
+
+@pyaccess.route("/")
+@jinja2_view("index.html")
+def index():
+    list_relais = {
+        1: "porte entrée (allumé = vérouillée, éteint = dévérouillée)",
+        2: "lumières enseignes",
+        3: "lumières out-receptions (30s pour s’éteindre)",
+        4: "lumières porches (extérieur)",
+        5: "lumières in-reception",
+        6: "lumières banque",
+        7: "lumières salle petit-déj buffet",
+        8: "lumières salle petit-déj tables",
+    }
+    state_relais = {relai: lock_door.is_on(relai) for relai in list_relais.keys()}
+    return {"list_relais":list_relais, "state_relais":state_relais}
+
+
+@pyaccess.route("/turn/<state>/<relai_ch>")
+def turn(state, relai_ch):
+    lock_door.turn(state, int(relai_ch))
+    resp = Response(f"{datetime.now().strftime('%H:%M:%S')} {relai_ch} : {state}")
+    resp.set_header('Access-Control-Allow-Origin', '*')
+    return resp
+
+lock_door.init_GPIO()
 
 from cheroot.wsgi import Server as WSGIServer, PathInfoDispatcher
+d = PathInfoDispatcher({"/": pyaccess})
+server = WSGIServer(("0.0.0.0", 8080), d)
 
-d = PathInfoDispatcher({'/': app})
-server = WSGIServer(('0.0.0.0', 5000), d)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         server.start()
     except KeyboardInterrupt:
         server.stop()
+
